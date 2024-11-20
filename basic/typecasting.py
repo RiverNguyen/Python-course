b1026f50e577d3491108bc6372af2ba27156d117
RiverNguyen <ngiang1309@gmail.com>
2024-11-20 15:04:33 +0700

    Upload py learn

---
+++ basic/typecasting.py
@@ -0,0 +1,11 @@
+# Typecasting is the process of converting one data type to another data type.
+
+name = "River Nguyen"
+age = 21
+gpa = 3.5
+is_student = True
+
+age = bool(age)
+
+print(age)
+
---
+++ basic/variables.py
@@ -0,0 +1,38 @@
+# This is my first python program
+# print("Hello World!")
+# print("It's really good")
+
+# Variables
+## String
+first_name = "River"
+food = "Pizza"
+email = "ngiang1309@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"You like {food}")
+print(f"Your email is: {email}")
+
+## Integers
+age = 21
+quantity = 2
+num_of_students = 30
+
+print(f"You are {age} years old")
+print(f"You have {quantity} items")
+print(f"Your class have {num_of_students} students")
+
+## Float
+price = 10.99
+gpa = 3.5
+
+print(f"The price is ${price}")
+print(f"Your GPA is: {gpa}/4")
+
+## Boolean
+is_student = True
+is_teacher = False
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are NOT a student")
